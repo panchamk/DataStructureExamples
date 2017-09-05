fb87378affe16c6e8695458b0ddd5162f3fcc7ac
Pancham Kumar Gupta <pancham_w8@hotmail.com>
2017-09-06 00:02:31 +0530

    Update Testmain.java

--- src/binaryTree/Testmain.java
+++ src/binaryTree/Testmain.java
@@ -1,10 +1,24 @@
 package binaryTree;
 
-public class Testmain {
+import binaryTree.helper.BinaryTreeConstructor;
+import binaryTree.helper.BinaryTreeTraversal;
+import binaryTree.pojo.BSTreeNode;
 
+/**
+ * @author pancham.gupta
+ *
+ */
+public class TestMain {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		BSTreeNode rootNode = BinaryTreeConstructor.constructBinaryTree();
+
+		System.out.println("{BinaryTree : " + rootNode + "}");
+		BinaryTreeTraversal.inOrderTraversal(rootNode);
+		System.out.println();
+		BinaryTreeTraversal.preOrderTraversal(rootNode);
+		System.out.println();
+		BinaryTreeTraversal.postOrderTraversal(rootNode);
 
 	}
-
 }
